ce0d429bad7d121a76e3faa0793e866580a000ce
Cotton <c.beckfield@pokemon.com>
2020-01-21 15:11:29 -0800

    This is almost working, need to fix an issue with the 403 forbidden issues and the formatting

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # jvquery
 Glossary for the infamous Visneski
+
+## Example Lambda Test
+```
+{
+  "token": "XXYYZZ",
+  "team_id": "TXXXXXXXX",
+  "api_app_id": "AXXXXXXXXX",
+  "event": {
+    "type": "name_of_event",
+    "event_ts": "1234567890.123456",
+    "text": "hot wash",
+    "user": "UXXXXXXX1",
+    "channel": "test_channel"
+  },
+  "type": "event_callback",
+  "authed_users": [
+    "UXXXXXXX1",
+    "UXXXXXXX2"
+  ],
+  "event_id": "Ev08MFMKH6",
+  "event_time": 1234567890
+}
+```
--- query.py
+++ query.py
@@ -1,48 +1,85 @@
 import os
 import logging
-import urllib
+import urllib.request
 
+# Phrases TO-DO
+#  Juice worth the squeeze
+# - Baby powder their feet
+# - At the end of the day
+# - Getting pregnant with other teams
+# - The sugar that helps make the medicine go down
+# - Taking away someone's birthday
+# - Making sure we're not destroying christmas
+# - Half pregnant
+# - They're a great American
+# - "Seems nice enough but I’m not exactly sure there is much going on between the ears "
+# - Press
+# - Dingus
+# - Let's rap about it on x day
+# - I drank his milkshake yesterday and he's probably a bit sore.
+# - mother fucking plato looking mother fucker
+# - Dropping the kids off at the pool
+# - Even I lose a marble or two once in a while.
 
+# Environmental Variables
 BOT_TOKEN = os.environ["BOT_TOKEN"]
-SLACK_URL = os.environ["POST_URL"]
+SLACK_URL = os.environ["SLACK_URL"]
 
 
 def lambda_handler(data, context):
 
-    slack_event = data['event']
+    print(data['event'])
+    if "challenge" in data:
+        return data["challenge"]
 
+    # Dictionary of Visneski-isms
+    jvdict = {
+        "hot wash":"lessons learned",
+        "press": "move forward"
+    }
+
+    # Handling some potential errors
+    slack_event = data['event']
     if "bot_id" in slack_event:
         logging.warn("Ignore bot event")
-    else:
-        text = slack_event["text"]
-        if text == "hot wash":
-            definition = "lessons learned"
-        end
+        return
 
-        channel_id = slack_event["channel"]
+    text = slack_event["text"]
+    if not text in jvdict:
+        logging.warn("Unknown phrase")
+        return
 
-        data = urllib.parse.urlencode(
-            (
-                ("token", BOT_TOKEN),
-                ("channel", channel_id),
-                ("term", text)
-                ("definition", definition)
-            )
+    # Setting up the definition, and searching the dict for the
+    # relevant word search.
+    definition = jvdict[text]
+    channel_id = slack_event["channel"]
+
+    # Format data and request values.
+    data = urllib.parse.urlencode(
+        (
+            ("token", BOT_TOKEN),
+            ("channel", channel_id),
+            ("term", text),
+            ("definition", definition)
         )
-        data = data.encode("ascii")
+    )
+    data = data.encode("ascii")
 
-        request = urllib.request.Request(
-            SLACK_URL,
-            data=data,
-            method="POST"
-        )
+    print(data)
 
-        request.add_header(
-            "Content-Type",
-            "application/x-www-form-urlencoded"
-        )
+    # Send definition back to Slack.
+    request = urllib.request.Request(
+        SLACK_URL,
+        data=data,
+        method="POST"
+    )
 
-        urllib.request.urlopen(request).read()
+    request.add_header(
+        "Content-Type",
+        "application/x-www-form-urlencoded"
+    )
+
+    urllib.request.urlopen(request).read()
 
     # Everything went fine.
     return "200 OK"
